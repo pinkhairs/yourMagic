c98eec0310dadb4eff051c927c385c3273f71b03
aminaeily <ami.naeily@zeek.com>
2022-08-23 22:55:21 -0600

    components collection

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,18 +2,23 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Components</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Components</ion-title>
         </ion-toolbar>
       </ion-header>
     
-      <ArticleBlock />
+      <article-block />
+      <spread-block />
+      <text-button width="block" />
+      <fake-door-button />
+      <calendar-month />
     </ion-content>
+    <circle-button vertical="bottom" horizontal="end" />
   </ion-page>
 </template>
 
@@ -21,9 +26,26 @@
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ArticleBlock from '@/components/Blocks/ArticleBlock.vue';
+import SpreadBlock from '@/components/Blocks/SpreadBlock.vue';
+import CircleButton from '@/components/Buttons/CircleButton.vue';
+import TextButton from '@/components/Buttons/TextButton.vue';
+import FakeDoorButton from '@/components/Buttons/FakeDoorButton.vue';
+import CalendarMonth from '@/components/Calendar/CalendarMonth.vue';
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: { ArticleBlock, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {
+    ArticleBlock,
+    SpreadBlock,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    CircleButton,
+    TextButton,
+    FakeDoorButton,
+    CalendarMonth
+  }
 });
 </script>
