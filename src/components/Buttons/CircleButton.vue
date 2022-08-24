39fd65d1c634c7f195a8b96cfc371c6bbca47c9c
aminaeily <ami.naeily@zeek.com>
2022-08-23 22:54:51 -0600

    buttons

---
+++ src/components/Buttons/CircleButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-fab :vertical="vertical" :horizontal="horizontal">
+    <ion-fab-button>
+      <ion-icon :icon="star"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+</template>
+
+<script>
+import { IonContent, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { star } from 'ionicons/icons';
+
+export default defineComponent({
+  components: { IonFab, IonFabButton, IonIcon },
+  props: ['vertical', 'horizontal'],
+  setup() {
+    return {
+      star
+    }
+  }
+});
+</script>
---
+++ src/components/Buttons/TextButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-button :expand="width">Text</ion-button>
+</template>
+
+<script>
+import { IonButton } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonButton },
+  props: [ 'width' ]
+});
+</script>
