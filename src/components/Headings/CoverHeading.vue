ee89b4f447cefb323f6b722058668af00a15d54f
aminaeily <ami.naeily@zeek.com>
2022-08-25 00:08:03 -0600

    heading components

---
+++ src/components/Headings/CoverHeading.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Good morning, Amí</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
---
+++ src/components/Headings/TextHeading.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 v-if="level === 2 || !level">{{title}}</h2>
+  <h3 v-if="level === 3">{{title}}</h3>
+  <h4 v-if="level === 4">{{title}}</h4>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: ['title', 'level']
+});
+</script>
