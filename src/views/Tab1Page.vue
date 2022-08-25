2a903115fa8c426a0514a9ea4d5d2030ed4efa84
aminaeily <ami.naeily@zeek.com>
2022-08-25 00:08:11 -0600

    add new components

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Components</ion-title>
+            <ion-title size="large">Components</ion-title>
         </ion-toolbar>
       </ion-header>
     
@@ -17,6 +17,11 @@
       <text-button width="block" />
       <fake-door-button />
       <calendar-month />
+      <card-container :flipped="true" />
+      <text-field />
+      <filter-list />
+      <cover-heading />
+      <text-heading title="Hi! How's it going?" />
     </ion-content>
     <circle-button vertical="bottom" horizontal="end" />
   </ion-page>
@@ -31,6 +36,11 @@ import CircleButton from '@/components/Buttons/CircleButton.vue';
 import TextButton from '@/components/Buttons/TextButton.vue';
 import FakeDoorButton from '@/components/Buttons/FakeDoorButton.vue';
 import CalendarMonth from '@/components/Calendar/CalendarMonth.vue';
+import CardContainer from '../components/Cards/CardContainer.vue';
+import TextField from '@/components/Fields/TextField.vue';
+import FilterList from '@/components/Filter/FilterList.vue';
+import CoverHeading from '@/components/Headings/CoverHeading.vue';
+import TextHeading from '@/components/Headings/TextHeading.vue';
 
 export default  defineComponent({
   name: 'Tab1Page',
@@ -45,7 +55,12 @@ export default  defineComponent({
     CircleButton,
     TextButton,
     FakeDoorButton,
-    CalendarMonth
+    CalendarMonth,
+    CardContainer,
+    TextField,
+    FilterList,
+    CoverHeading,
+    TextHeading
   }
 });
 </script>
