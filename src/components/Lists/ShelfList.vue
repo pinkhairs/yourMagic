8d4b457570a94955d77396a8774316dc38af255d
aminaeily <ami.naeily@zeek.com>
2022-08-25 00:14:25 -0600

    list components

---
+++ src/components/Lists/ShelfList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-list>
+    <ion-item>1</ion-item>
+    <ion-item>2</ion-item>
+    <ion-item>3</ion-item>
+  </ion-list>
+</template>
+
+<script>
+import { IonList, IonItem } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonList, IonItem },
+});
+</script>
