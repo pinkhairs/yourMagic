ef0f9f148f3fa8cfa01eab9dae250de5f1bbacfa
aminaeily <ami.naeily@zeek.com>
2022-08-25 00:07:26 -0600

    filter buttons

---
+++ src/components/Filter/FilterButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <ion-button>Text</ion-button>
+</template>
+
+<script>
+import { IonButton } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonButton }
+});
+</script>
---
+++ src/components/Filter/FilterList.vue
@@ -0,0 +1,16 @@
+<template>
+  <filter-button />
+  <filter-button />
+  <filter-button />
+  <filter-button />
+  <filter-button />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import FilterButton from './FilterButton.vue';
+
+export default defineComponent({
+  components: { FilterButton }
+});
+</script>
