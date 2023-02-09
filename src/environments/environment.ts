fd9233e734490ea92a7699957770feb293db5248
pinkhairs <me@diana.nu>
2023-02-09 14:27:50 -0700

    remove env from environment ts file

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://dianal48.sg-host.com/wp-json/',
-    jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaXNzIjoiZGlhbmFsNDguc2ctaG9zdC5jb20ifQ.VWYbcNINDvub7V3a1cuPcYilvFvzyar3X-3yM5d5Kh8',
-    openAi: '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'
-}
